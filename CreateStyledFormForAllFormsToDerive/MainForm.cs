7c0333721f0ed91e82363f3dec7a3dbc558444ac
Kulpot <kulpotwahu@gmail.com>
2023-05-31 06:15:55 +0800

    Add project files.

---
+++ CreateStyledFormForAllFormsToDerive/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//ref link:https://www.youtube.com/watch?v=MmRr1Jka42o&list=PLAIBPfq19p2EJ6JY0f5DyQfybwBGVglcR&index=77
+//Saves time in a themed application. You can also use this for other things that can be consistent in your forms.
+
+namespace CreateStyledFormForAllFormsToDerive
+{
+    public partial class MainForm : StyledForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CreateStyledFormForAllFormsToDerive/StyledForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateStyledFormForAllFormsToDerive
+{
+    public partial class StyledForm : Form
+    {
+        private LinearGradientBrush _LGB;
+
+        public StyledForm()
+        {
+            InitializeComponent();
+            _LGB = new LinearGradientBrush(this.ClientRectangle, Color.Purple, Color.MediumPurple, 45f);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.FillRectangle(_LGB, this.ClientRectangle);
+        }
+    }
+}
